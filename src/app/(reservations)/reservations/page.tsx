df13725d2e1ca55fe8f8f25334bf29980adbba68
jamestariga <jamestariga08@live.com>
2024-12-26 19:16:30 -0800

    🔥 Updated the query

--- src/app/(reservations)/reservations/page.tsx
+++ src/app/(reservations)/reservations/page.tsx
@@ -1,12 +1,6 @@
 import { createClient } from '@/supabase/auth/server'
-import ListView from '@/components/ListView/ListView'
-import { Reservation } from '@/features/reservations/types/reservations.types'
-import { format } from 'date-fns'
-
-type ProfileAndReservations = {
-  id: number
-  reservations: Reservation[]
-}
+import { ProfileAndReservations } from '@/features/reservations/types/reservations.types'
+import ReservationsList from '@/features/reservations/ReservationsList'
 
 const Reservations = async () => {
   const supabase = await createClient()
@@ -17,41 +11,34 @@ const Reservations = async () => {
     .from('profiles')
     .select(
       `
-        id,
-        reservations:reservations(*)
-      `
+      id,
+      display_name,
+      reservations:reservations(
+        *,
+        barber:barbers(
+          id,
+          profile:profiles(
+            id,
+            display_name
+          )
+        )
+      )
+    `
     )
     .eq('user_id', userId)
     .single<ProfileAndReservations>()
 
-  if (error) {
-    console.error('Error fetching data:', error)
-  }
-
   console.log(profileAndReservations)
 
-  const formatDateTime = (dateString: string) => {
-    // Add 8 hours to convert from PST to UTC display
-    const date = new Date(dateString)
-    date.setTime(date.getTime() + 8 * 60 * 60 * 1000) // Add 8 hours
-    return format(date, "MMM d, yyyy 'at' h:mm a 'PST'")
+  if (error) {
+    console.error('Error fetching data:', error)
   }
 
   return (
     <div className='mx-auto'>
       <h1>Reservations Page</h1>
       {profileAndReservations && (
-        <ListView
-          data={profileAndReservations.reservations}
-          renderItem={(item) => (
-            <div key={item.id}>
-              <h2>{item.service.service_name}</h2>
-              <p>Start: {formatDateTime(item.start)}</p>
-              <p>End: {formatDateTime(item.end)}</p>
-              <p>Status: {item.status}</p>
-            </div>
-          )}
-        />
+        <ReservationsList reservations={profileAndReservations.reservations} />
       )}
     </div>
   )
