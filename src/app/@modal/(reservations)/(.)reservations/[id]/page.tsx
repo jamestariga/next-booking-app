fd52010cce07352b2f449fd2d50363cc3a9ad73b
jamestariga <jamestariga08@live.com>
2025-01-06 17:24:01 -0800

    🔥 Added a parallel route for reservations by id

---
+++ src/app/@modal/(reservations)/(.)reservations/[id]/page.tsx
@@ -0,0 +1,24 @@
+import Appointments from '@/features/appointments/Appointments'
+import { createClient } from '@/supabase/auth/server'
+import { Appointment } from '@/features/reservations/types/reservations.types'
+
+type Params = Promise<{ id: number }>
+
+const ReservationModal = async ({ params }: { params: Params }) => {
+  const { id } = await params
+  const supabase = await createClient()
+
+  const { data: appointment } = await supabase
+    .from('reservations')
+    .select('*')
+    .eq('id', id)
+    .single<Appointment>()
+
+  if (!appointment) {
+    return <div>Loading Page...</div>
+  }
+
+  return <Appointments data={appointment} />
+}
+
+export default ReservationModal
