c0a7886902c16b7d83ca8b31751752cc77aa9308
jamestariga <jamestariga08@live.com>
2025-01-16 17:00:11 -0800

    🔥 Added a temp card

--- src/app/(reservations)/appointments/[id]/page.tsx
+++ src/app/(reservations)/appointments/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { createClient } from '@/supabase/auth/server'
 import { Appointment } from '@/features/reservations/types/reservations.types'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { formatDateTime } from '@/lib/utils'
 
 type Params = Promise<{ id: number }>
 
@@ -13,7 +16,24 @@ const AppointmentPage = async ({ params }: { params: Params }) => {
     .eq('id', id)
     .single<Appointment>()
 
-  return <div>Page: {id}</div>
+  return (
+    <div className='max-w-3xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-bold'>Appointment</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>{appointment?.service.service_name}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Date and Time: {formatDateTime(appointment?.start ?? '')}</p>
+          <p>Price: {appointment?.service.price}</p>
+          <div className='flex w-full gap-2 py-4 justify-between'>
+            <Button variant='destructive'>Cancel Appointment</Button>
+            <Button>Confirm Appointment</Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
 export default AppointmentPage
