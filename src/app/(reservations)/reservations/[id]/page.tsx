b55fdbc8827b168e14ed103948868c175e7da3e5
jamestariga <jamestariga08@live.com>
2025-01-17 17:44:51 -0800

    🔥 Added a temp card

--- src/app/(reservations)/reservations/[id]/page.tsx
+++ src/app/(reservations)/reservations/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/supabase/auth/server'
 import { Appointment } from '@/features/reservations/types/reservations.types'
-
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { formatDateTime } from '@/lib/utils'
 type Params = Promise<{ id: number }>
 
 const ReservationPage = async ({ params }: { params: Params }) => {
@@ -13,7 +14,22 @@ const ReservationPage = async ({ params }: { params: Params }) => {
     .eq('id', id)
     .single<Appointment>()
 
-  return <div>Page: {id}</div>
+  // TODO - Add a function that confirms the status of the appointment
+
+  return (
+    <div className='max-w-3xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-bold'>Reservations</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>{appointment?.service.service_name}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{formatDateTime(appointment?.start ?? '')}</p>
+          <p>{appointment?.service.price}</p>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
 export default ReservationPage
