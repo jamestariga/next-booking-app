006f7a694874ed6840050ec94d877c2e5b625565
jamestariga <jamestariga08@live.com>
2025-01-18 17:02:59 -0800

    🔥 Moved the time and date formatter to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { format } from 'date-fns'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDateTime(dateString: string) {
+  // Add 8 hours to convert from PST to UTC display
+  const date = new Date(dateString)
+  date.setTime(date.getTime() + 8 * 60 * 60 * 1000) // Add 8 hours
+  return format(date, "MMM d, yyyy 'at' h:mm a 'PST'")
 }
