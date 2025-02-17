48ed95bde6a9051cd241d8e25b3cdeb37a2a7eac
jamestariga <jamestariga08@live.com>
2025-02-16 22:59:37 -0800

    🔥 Added states for the appointment modal

--- src/features/appointments/Appointments.tsx
+++ src/features/appointments/Appointments.tsx
@@ -1,11 +1,19 @@
 'use client'
 
+import { useActionState } from 'react'
+import { updateAppointment } from '@/server-actions/reservations'
 import Modal from '@/components/Modal/Modal'
-import { Appointment } from '@/features/reservations/types/reservations.types'
+import {
+  Appointment,
+  AppointmentStatus,
+} from '@/features/reservations/types/reservations.types'
 import {
   IBarberProfile,
   IProfile,
 } from '@/features/appointments/types/appointments.types'
+import AppointmentForm from './components/AppointmentForm'
+import { State } from '../auth/types/auth.types'
+import useRedirectOnSuccess from '@/hooks/useRedirectOnSuccess'
 
 type Props = {
   type: 'appointment' | 'reservation'
@@ -14,24 +22,47 @@ type Props = {
   clientData?: IProfile
 }
 
+const initialState: State = {
+  isOpen: true,
+  success: false,
+}
+
 const Appointments = ({ type, data, barberData, clientData }: Props) => {
+  const [state, formAction] = useActionState<State, AppointmentStatus>(
+    updateAppointment,
+    initialState
+  )
   const displayType = type === 'appointment' ? 'Customer' : 'Barber'
   const displayName =
     type === 'appointment'
       ? clientData?.display_name
       : barberData?.profiles?.display_name
 
+  useRedirectOnSuccess(
+    state?.success,
+    `/appointments`,
+    'Updated Successfully!',
+    true
+  )
+
+  console.log(state)
+
   return (
     <Modal
       title='Appointment'
       description='Book your next visit'
-      isOpen={true}
+      isOpen={state.isOpen ?? true}
       link='/'
       linkTitle='View Appointments'
     >
-      <div>Service: {data?.service.service_name}</div>
-      <div>{`${displayType}: ${displayName} `}</div>
-      <div>Status: {data?.status}</div>
+      <AppointmentForm
+        id={data.id}
+        service={data?.service.service_name}
+        type={displayType}
+        status={data?.status}
+        displayName={displayName}
+        action={formAction}
+      />
     </Modal>
   )
 }
---
+++ src/features/appointments/components/AppointmentForm.tsx
@@ -0,0 +1,81 @@
+import { useState, startTransition } from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { AppointmentStatus } from '@/features/reservations/types/reservations.types'
+
+type Props = {
+  id: number
+  service: string
+  type: 'Customer' | 'Barber'
+  status: string
+  displayName: string | undefined
+  action: (payload: AppointmentStatus) => void
+}
+
+const AppointmentForm = ({
+  id,
+  service,
+  type,
+  status,
+  displayName,
+  action,
+}: Props) => {
+  const [currentStatus, setCurrentStatus] = useState<string>(status)
+  const [editable, setEditable] = useState<boolean>(false)
+
+  const onSelectStatus = (value: string) => {
+    setCurrentStatus(value)
+  }
+
+  const updateAndClose = (id: number, newStatus: string) => {
+    startTransition(() => {
+      action({ id, status: newStatus })
+      setEditable(false)
+    })
+  }
+
+  return (
+    <>
+      {!editable ? (
+        <div className='flex flex-col w-full gap-2 py-4 justify-between'>
+          <div>
+            <p>Service: {service}</p>
+            <p>{`${type}: ${displayName}`}</p>
+            <p>Status: {currentStatus}</p>
+          </div>
+          <Button onClick={() => setEditable(true)}>Update Status</Button>
+        </div>
+      ) : (
+        <div className='flex flex-col w-full gap-2 py-4'>
+          <h2 className='text-[hsl(var(--muted-foreground))]'>Update Status</h2>
+          <Select onValueChange={onSelectStatus} value={currentStatus}>
+            <SelectTrigger>
+              <SelectValue placeholder='Select a status' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='pending'>Pending</SelectItem>
+              <SelectItem value='confirmed'>Confirmed</SelectItem>
+              <SelectItem value='cancelled'>Cancelled</SelectItem>
+            </SelectContent>
+          </Select>
+          <div className='w-full space-x-2'>
+            <Button onClick={() => updateAndClose(id, currentStatus)}>
+              Update Status
+            </Button>
+            <Button onClick={() => setEditable(false)} variant='destructive'>
+              Cancel
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default AppointmentForm
